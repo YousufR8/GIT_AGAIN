63e0c1913f93c4ce758ce72fdd2649ada3f31f15
Rehman Yousuf <Rehman_Yousuf@student.MahoningCTC.com>
2022-09-07 11:56:13 -0400

    asdf

---
+++ Week 2/calculation.py
@@ -0,0 +1,31 @@
+#Addition
+my_addition = 3 + 5
+print(my_addition)
+
+my_subtraction = 37-83
+print(my_subtraction)
+
+my_mutiply = 5*4567
+print(my_mutiply)
+
+my_division = 87 / 3
+print(my_division)
+
+number1 = (input("Enter a number to check if it is divisible by 3:"))
+
+divisible_by_3 = number1 / 3
+print(divisible_by_3)
+
+#Modulo
+divisible_by_3 =27%4
+print =(divisible_by_3)
+
+# Exponents
+my_exponents = 3**4
+print(my_exponents)
+
+my_favorite_number = int(input("Enter Your Favorite Number: "))
+print(f"your favorite number is: {my_favorite_number}")
+
+my_favorite_number = int(input("Enter your Favorite Number:"))
+print(f"Your favorite number is: {my_favorite_number}")
